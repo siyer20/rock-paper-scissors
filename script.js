acbd27d310bd8077478242f10a0026ae59e65bd8
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-23 20:04:53 -0500

    Created getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello world");
+
+function getComputerChoice(){
+    switch(Math.floor(Math.random()*10)%3){
+        case 0:
+            return "rock"
+        case 1:
+            return "paper"
+        case 2:
+            return "scissors"
+    }
+}
