41870e90ba7c2d53b0950a03981360b1ae0689bc
siyer20 <90734516+siyer20@users.noreply.github.com>
2025-12-30 16:21:38 -0500

    Commented out original JS code

--- script.js
+++ script.js
@@ -11,44 +11,44 @@ function getComputerChoice(){
 
 }
 
-//function to get user's play
-let getHumanChoice = () => prompt("What's your play?");
+// //function to get user's play
+// let getHumanChoice = () => prompt("What's your play?");
 
-//function for a single round of gameplay
-function playRound (humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase().trim();
+// //function for a single round of gameplay
+// function playRound (humanChoice, computerChoice){
+//     humanChoice = humanChoice.toLowerCase().trim();
 
-    if (humanChoice == computerChoice){
-        return console.log("It's a tie!");
-    }
-    else if ((humanChoice == "rock" && computerChoice == "scissors") ||
-             (humanChoice == "paper" && computerChoice == "rock") ||
-             (humanChoice == "scissors" && computerChoice == "paper")){
-        humanScore++;
-        return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
-    }
-    else if ((humanChoice == "rock" && computerChoice == "paper") ||
-             (humanChoice == "paper" && computerChoice == "scissors") ||
-             (humanChoice == "scissors" && computerChoice == "rock")){
-        compScore++;
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
-    }
-    else{
-        return console.log("Invalid input! Please enter rock, paper, or scissors.");
-    }
-}
+//     if (humanChoice == computerChoice){
+//         return console.log("It's a tie!");
+//     }
+//     else if ((humanChoice == "rock" && computerChoice == "scissors") ||
+//              (humanChoice == "paper" && computerChoice == "rock") ||
+//              (humanChoice == "scissors" && computerChoice == "paper")){
+//         humanScore++;
+//         return console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+//     }
+//     else if ((humanChoice == "rock" && computerChoice == "paper") ||
+//              (humanChoice == "paper" && computerChoice == "scissors") ||
+//              (humanChoice == "scissors" && computerChoice == "rock")){
+//         compScore++;
+//         return console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+//     }
+//     else{
+//         return console.log("Invalid input! Please enter rock, paper, or scissors.");
+//     }
+// }
 
-//declaring global score variables
-let humanScore = 0;
-let compScore=0;
+// //declaring global score variables
+// let humanScore = 0;
+// let compScore=0;
 
-//loop to play this game!
+// //loop to play this game!
 
-console.log("Welcome to Rock, Paper, Scissors! First to 5 wins!")
+// console.log("Welcome to Rock, Paper, Scissors! First to 5 wins!")
 
-while (humanScore < 5 && compScore < 5){
-    let humanChoice = getHumanChoice();
-    let computerChoice = getComputerChoice();
-    playRound(humanChoice, computerChoice);
-    console.log(`Score - You: ${humanScore}, Computer: ${compScore}`);
-}
+// while (humanScore < 5 && compScore < 5){
+//     let humanChoice = getHumanChoice();
+//     let computerChoice = getComputerChoice();
+//     playRound(humanChoice, computerChoice);
+//     console.log(`Score - You: ${humanScore}, Computer: ${compScore}`);
+// }
